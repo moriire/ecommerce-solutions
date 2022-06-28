78fc0da54609a3c733d9b0abd5ff48cc79b155f6
moriire <ibraheemabdulsalam@gmail.com>
2022-06-28 20:22:04 +0100

    users update

--- shop/users/admin.py
+++ shop/users/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Users#, UserManager
+
+admin.site.register(Users)#, UserManager)
 
 # Register your models here.
--- shop/users/views.py
+++ shop/users/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from urllib.parse import urlparse, urlunparse
 
-# Create your views here.
+from django.conf import settings
+
+# Avoid shadowing the login() and logout() views below.
+from django.contrib.auth import REDIRECT_FIELD_NAME, get_user_model
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import (
+    AuthenticationForm,
+    PasswordChangeForm,
+    PasswordResetForm,
+    SetPasswordForm,
+)
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.exceptions import ImproperlyConfigured, ValidationError
+from django.http import HttpResponseRedirect, QueryDict
+from django.shortcuts import resolve_url
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.utils.http import url_has_allowed_host_and_scheme, urlsafe_base64_decode
+from django.utils.translation import gettext_lazy as _
+from django.views.decorators.cache import never_cache
+from django.views.decorators.csrf import csrf_protect
+from django.views.decorators.debug import sensitive_post_parameters
+from django.views.generic.base import TemplateView
+from django.views.generic.edit import FormView
+
+#UserModel = get_user_model()
+
+
+
