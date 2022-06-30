3085eaee062134e1433cb25f29772ca4dadcccb8
moriire <ibraheemabdulsalam@gmail.com>
2022-06-30 07:29:55 +0100

    reset

--- shop/shop/urls.py
+++ shop/shop/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.static import static                                from django.conf import settings
+#from django.conf.urls.static import static                                from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 import product
@@ -9,8 +9,8 @@ urlpatterns = [
     path('api/', include('product.urls')),
     path('admin/', admin.site.urls),
 ]
-
+"""
 if settings.DEBUG:
     urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     #urlpatterns = urlpattern
-
+"""
