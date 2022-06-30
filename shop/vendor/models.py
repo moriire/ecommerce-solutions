273dfefa453cab09461189e7942a83a63394e581
moriire <ibraheemabdulsalam@gmail.com>
2022-06-30 11:26:31 +0100

    update models.py

--- shop/vendor/models.py
+++ shop/vendor/models.py
@@ -14,7 +14,7 @@ class VendorSerializer(serializers.ModelSerializer):
 		model = Profile 
 		fields = "__all__"
 
-class VendorUserSerializer(serializer.ModelSerializer):
+class VendorUserSerializer(serializers.ModelSerializer):
 	user = Users.objects.all()
 	class Meta:
 		model = Profile 
