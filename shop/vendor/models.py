8848593ed6191eaffe1d7be60281a60286a4f63f
moriire <ibraheemabdulsalam@gmail.com>
2022-06-30 11:48:16 +0100

    update models.py

--- shop/vendor/models.py
+++ shop/vendor/models.py
@@ -3,26 +3,24 @@ from users.models import Users
 from rest_framework import serializers
 
 class Vendor(models.Model):
-	user = models.OneToOneField(Users, on_delete=models.DO_NOTHING, related_name="user_profile")
-	vendor = models.CharField(max_length=100, unique=True)
-	address = models.TextField()
-	merchandise = models.TextField()
-    
+    user = models.OneToOneField(Users, on_delete=models.DO_NOTHING, related_name="user_profile")
+    vendor = models.CharField(max_length=100, unique=True)
+    address = models.TextField()
+    merchandise = models.TextField()
     def __str__(self):
         return "{}-{}".format(self.user.phone, self.vendor)
 
-
-
 class VendorSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Vendor 
-		fields = "__all__"
+    class Meta:
+        model = Vendor 
+        fields = "__all__"
+
 
 class VendorUserSerializer(serializers.ModelSerializer):
-	user = Users.objects.all()
-	class Meta:
-		model = Vendor 
-		fields = (
+    user = Users.objects.all()
+    class Meta:
+        model = Vendor
+        fields = (
                 "user", 
                 "vendor", 
                 "address", 
