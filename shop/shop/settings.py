85006c6267c268b389c4f258c1600e9784fb33d4
moriire <ibraheemabdulsalam@gmail.com>
2022-07-03 17:45:14 +0100

    frontend update

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "rest_framework",
+    "corsheaders",
     "users",
     "vendor",
     "category",
@@ -50,6 +51,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -132,6 +134,11 @@ STATICFILES_DIRS = ["static"]
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "https://ecs.pythonanywhere.com",
+    "http://127.0.0.1:8080",
+]
 
 
 
