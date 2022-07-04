3065d1a5271878d0573385137f49ab2de7b0c2b8
moriire <ibraheemabdulsalam@gmail.com>
2022-07-04 23:42:08 +0100

    settings.py

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = '/home/ecs/ecommerce-solutions/shop/static'
 
-STATICFILES_DIRS = ["static"]
+#STATICFILES_DIRS = ["static"]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
