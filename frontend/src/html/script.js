e52baacc5d6ffdb392941b7d0d90d574c61e82da
Mobolaji124 <ibraheemabdulsalam@gmail.com>
2022-09-09 06:34:12 -0700

    index login signup

--- frontend/src/html/script.js
+++
@@ -1,8 +0,0 @@
-// let nav1 = document.getElementById('nav1');
-// window.onscroll = () => {
-//     if (window.pageYOffset >= menu.offsetTop){
-//         nav1.classList.add(fixed)
-//     }
-// } 
-
-
