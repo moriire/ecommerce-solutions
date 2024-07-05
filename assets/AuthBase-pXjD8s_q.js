c7d7b42bf97d21c59f8b3e9a950d2a6b2aca0211
IBM Abdulsalam <ibmabdulsalam@gmail.com>
2024-07-05 01:11:58 +0100

    Updates

---
+++ assets/AuthBase-pXjD8s_q.js
@@ -0,0 +1,1 @@
+import{a as e,c as t,l as a,h as o,z as n}from"./index-Bexpoaz0.js";const s={id:"MainContent",class:"content-for-layout mb-4"},i={__name:"AuthBase",setup(c){return(r,_)=>(e(),t("main",s,[a(o(n))]))}};export{i as default};
