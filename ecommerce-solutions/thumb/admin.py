deb4bca6f4423ea278d06b8461245d6685e91e58
IBM Abdulsalam <ibmabdulsalam@gmail.com>
2024-08-13 11:10:22 +0100

    latest

--- ecommerce-solutions/thumb/admin.py
+++ ecommerce-solutions/thumb/admin.py
@@ -9,7 +9,14 @@ class ProductImageAdmin(admin.ModelAdmin):
     list_display = ['product', 'image_tag', 'uploaded_on']
 
 class ProductWithImageAdmin(admin.ModelAdmin):
-    list_display = ['product', 'images', 'status']
+    def images_tag(self, obj):
+        imgs = obj.images.all()
+        li = ""
+        for img in imgs:
+            print(img.img)
+            li+='<img src="{}" style="max-width:150px; max-height:150px; margin:10px;"/>'.format(img.img.url)            
+        return format_html(li)# format_html('<img src="{}" style="max-width:200px; max-height:200px"/>'.format(obj.images.all()[0].img.url))
+    list_display = ['product', "images_tag", 'status']
 
 admin.site.register(ProductImage, ProductImageAdmin)
-admin.site.register(ProductWithImage)#, ProductWithImageAdmin)
+admin.site.register(ProductWithImage, ProductWithImageAdmin)
